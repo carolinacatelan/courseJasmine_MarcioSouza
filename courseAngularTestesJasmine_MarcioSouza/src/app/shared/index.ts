686d144454cbb8bb7d0be5792a33416d09406741
anacarolinacs <anacarolinacs@ciandt.com>
2022-05-25 10:29:02 -0300

    Primeiro commit

---
+++ courseAngularTestesJasmine_MarcioSouza/src/app/shared/index.ts
@@ -0,0 +1,8 @@
+export * from "./pipes";
+export * from "./utils";
+export * from "./validators";
+export * from "./directives";
+export * from "./components";
+export * from "./shared.module";
+export * from "./testing";
+
